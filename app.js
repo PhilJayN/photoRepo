d6ff502e182ef362fbcdeefa5cffc754e14b35f3
Philip Nguyen <phi.nguyen@laverne.edu>
2017-03-23 12:51:47 -0700

    Add preliminary mongoose connect to DB

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
 var express = require("express");
 var app = express();
 
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/photos_app");
+
+var photoSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+
+var Photo = mongoose.model("Photo", photoSchema);
+
+//add to DB:
+var sunset = new Photo({
+  name: "Sunset",
+  image: "Url image",
+  description: "Beautiful orange sunset"
+});
+
+sunset.save(function (err, photo ) {
+  if (err) {
+    console.log('ERR!');
+  } else {
+    console.log('Saved to db!!');
+    console.log('here is photo', photo);
+  }
+});
+
+Photo.find({}, function(err, photos){
+  if (err) {
+    console.log('errrrr!');
+  } else {
+    console.log(photos);
+  }
+});
+
 app.get('/', function (req, res) {
   console.log('landing pg!');
   res.render('landing.ejs');
@@ -8,6 +43,10 @@ app.get('/', function (req, res) {
 
 app.get('/photos', function (req, res) {
   res.render('photos.ejs');
+});
+
+app.get('/photos/new', function (req, res) {
+  res.render('new.ejs');
 });
 
 app.get('*', function (req, res) {
