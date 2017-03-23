aa239f26d012f9014438056c96ee663a3a4ae8d2
Philip Nguyen <phi.nguyen@laverne.edu>
2017-03-22 21:48:12 -0700

    Add ejs package

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require("express");
 var app = express();
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   console.log('landing pg!');
+  res.render('landing.ejs');
 });
 
+app.get('*', function (req, res) {
+  res.redirect('/');
+});
 
 app.listen(3000, function () {
   console.log('listening on port 3000!');
