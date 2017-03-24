a86e3d267b1af4f77dadddddfb0e4f49e98dc338
Philip Nguyen <phi.nguyen@laverne.edu>
2017-03-23 17:25:45 -0700

    Add post routes

--- app.js
+++ app.js
@@ -13,18 +13,22 @@ var photoSchema = new mongoose.Schema({
 var Photo = mongoose.model("Photo", photoSchema);
 
 //add to DB:
-Photo.create({
-  name: "red",
-  image: "http://placekitten.com.s3.amazonaws.com/homepage-samples/200/287.jpg",
-  description: "description"
-}, function(err, photo) {
-  if (err) {
-    console.log("ERR");
-  } else {
-    console.log(photo);
-  }
-});
+// Photo.create({
+//   name: "red",
+//   image: "http://placekitten.com.s3.amazonaws.com/homepage-samples/200/287.jpg",
+//   description: "description"
+// }, function(err, photo) {
+//   if (err) {
+//     console.log("ERR");
+//   } else {
+//     console.log(photo);
+//   }
+// });
 
+//ROUTES
+app.get('/404', function (req, res) {
+  res.render('pagenotfound.ejs');
+});
 
 app.get('/', function (req, res) {
   console.log('landing pg!');
@@ -42,14 +46,28 @@ app.get('/photos', function (req, res) {
   });
 });
 
-app.post('/photos/new', function (req, res) {
-  // res.render('new.ejs');
-
+app.get('/photos/new', function(req, res){
+  res.render('new.ejs');
 });
 
+//add to DB on submit btn click:
+app.post('/photos', function (req, res) {
+  var name = req.body.name;
+  var image = req.body.image;
+  var description = req.body.description;
+  //takes data from variables name and image, and stores into an obj:
+  var newImage = {name: name, image: image, description: description};
+  Photo.create(newImage, function(err, newlyCreated){
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/photos');
+    }
+  });
+});
 
 app.get('*', function (req, res) {
-  res.redirect('/');
+  res.redirect('/404');
 });
 
 app.listen(3000, function () {
