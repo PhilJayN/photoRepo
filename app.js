9e008601a04f958cb4f2d3f3dfaf6fcc085e34a8
Philip Nguyen <phi.nguyen@laverne.edu>
2017-03-28 19:28:42 -0700

    Add user.js exports

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
